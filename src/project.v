ca0f76fa996fe1c5b92a2cd8f18d99c1e2082807
magnetofield <34056090+magnetoField@users.noreply.github.com>
2026-05-06 13:51:55 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_okforth_subleq_cpu (
+module tt_um_okforth_ieee (
 	input  wire [7:0] ui_in,	// Dedicated inputs
 	output wire [7:0] uo_out,	// Dedicated outputs
 	input  wire [7:0] uio_in,	// IOs: Input path
